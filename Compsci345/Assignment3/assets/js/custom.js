ef33d378ec6955ee79cf17f737ad5116bf8cc7d8
KieranSScott <kewanscott@gmail.com>
2017-05-28 19:11:49 +1200

    Final version for assignment 3

--- Compsci345/Assignment3/assets/js/custom.js
+++ Compsci345/Assignment3/assets/js/custom.js
@@ -1,4 +1,25 @@
-function disableTextArea(el) {
+function disableTextArea() {
+    $('#c1q1,#answer_c1q1').attr("disabled", "disabled");
+}
+
+function change_tab(el) {
     caller = $(el);
-    document.getElementById(caller).disabled = true;
+
+    caller.parent().find(".tab-controls").removeClass("active");
+    caller.addClass('active');
+
+    tab_ref = caller.attr('data-ref');
+    tab_group = caller.parent().attr('data-tabref');
+
+    console.log(tab_ref);
+    console.log(tab_group);
+
+    $("#" + tab_group + " .active-tab").removeClass("active-tab");
+    $("#" + tab_group + " #" + tab_ref).addClass("active-tab");
+}
+
+function addResponse(el1, el2) {
+    var response = $("#c1q1_response").val();
+    $("#c1q1_add").append(response + "<br/>" + "<p></p>");
+
 }
